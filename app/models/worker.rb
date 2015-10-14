65fe96d2bb6b6a7fd6461337c160ab4251d4cddf
Jonathan Medina <jom901@gmail.com>
2015-10-14 12:12:19 -0400

    Added paperclip and created a workers display page. Still have to work on images

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -3,4 +3,6 @@ class Worker < ActiveRecord::Base
 	validates :email, presence: true
 	validates :bio, presence: true
 	validates :role, presence: true
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006201855) do
+ActiveRecord::Schema.define(version: 20151014160840) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -27,9 +27,13 @@ ActiveRecord::Schema.define(version: 20151006201855) do
     t.string   "email"
     t.string   "twitter"
     t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "role"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
